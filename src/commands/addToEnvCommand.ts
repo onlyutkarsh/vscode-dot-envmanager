0ec847949a089fd4f28b473d15bfe70f2d698c0c
Utkarsh Shigihalli <onlyutkarsh@users.noreply.github.com>
2022-06-26 18:13:52 +0100

    generate env variable

--- src/commands/addToEnvCommand.ts
+++ src/commands/addToEnvCommand.ts
@@ -1,7 +1,17 @@
 import { window } from "vscode";
 import { getSelectedText } from "../handlers/activeEditorHandler";
-
+import { toEnvironmentVariable } from "../utilities/stringUtilities";
 export async function addToEnv() {
   let selected = getSelectedText();
+  let envVariable = toEnvironmentVariable(selected);
+  let envLine = `${envVariable}=${selected}`;
+  window.showInputBox({
+    ignoreFocusOut: true,
+    placeHolder: envLine,
+    prompt: "Enter a name for the environment variable",
+    title: "Add to .env",
+    value: envLine,
+    valueSelection: [0, envVariable.length],
+  });
   window.showInformationMessage(`Added ${selected} to .env`);
 }
--- src/utilities/stringUtilities.ts
+++ src/utilities/stringUtilities.ts
@@ -1,3 +1,8 @@
 export function toUpper(input: string): string {
   return input.toUpperCase();
 }
+
+export function toEnvironmentVariable(input: string): string {
+  let envVariable = input.replace(/['"]+/g, "").replace(" ", "_");
+  return envVariable.toUpperCase();
+}
